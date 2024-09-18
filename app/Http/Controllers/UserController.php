cae081444d800e09d1e82995c864c501dcdae3c7
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-09-18 10:49:58 +0700

    JS04-Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,50 @@ class UserController extends Controller
 {
     public function index() 
     {
-        $user = UserModel::firstOrNew(
+        // praktikum 2.5 no 1
+        /* $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager55',
+                'nama' => 'Manager55',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty('nama', 'username'); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean('nama', 'username'); // false
+
         $user->save();
 
-        return view('user', ['data' => $user]);
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty()); */
+
+        // praktikum 2.5 no 3
+        $user = UserModel::create(
+            [
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged('username', 'level_id'); // true
+        $user->wasChanged('nama'); // flase
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
