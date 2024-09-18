e3a040a92881f89a2a972aaa02d0e31cd5504543
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-09-18 08:57:47 +0700

    JS04-Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,8 @@ class UserController extends Controller
 {
     public function index() 
     {
-        // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1)->first();
-        // $user = UserModel::firstWhere('level_id', 1);
-        
-        $user = UserModel::findOr(20, ['username','nama'], function() {
-            abort(404);
-        });
-
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
