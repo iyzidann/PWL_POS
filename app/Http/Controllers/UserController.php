52a96ef4416a66b47cc20fc7b4ff769eb6c90a3c
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-09-18 08:52:44 +0700

    JS04-Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,30 +10,14 @@ class UserController extends Controller
 {
     public function index() 
     {
-        // tambah data user dengan Eloquent Model
-        /* $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
-        ];
-        UserModel::insert($data); */ // tambahkan data ke tabel m_user
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        
+        $user = UserModel::findOr(20, ['username','nama'], function() {
+            abort(404);
+        });
 
-        /* $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); */ // update data user
-
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
