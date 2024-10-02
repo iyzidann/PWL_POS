7aba1943a54e700d20484b5dbab79eff3bb02644
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-10-02 10:49:02 +0700

    JS06-Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::group(['prefix' => 'user'], function() {
     Route::put('/{id}', [UserController::class, 'update']);     // menyimpan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);    // menampilkan halaman form edit user ajax
     Route::put('{id}/update_ajax', [UserController::class, 'update_ajax']); //m menyimpan perubahan data user ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);   // untuk tampilkan form confirm delete user ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // untuk menghapus data user ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
